b0f2d40ce8dcd13868f790d152ba9474474a1dcc
bash360 <mark.bashir.ugwi@gmail.com>
2025-11-18 15:41:11 +0100

    added retry
    added retry methods execute to call operation and retry if operation fails

--- internal/retry/retry.go
+++ internal/retry/retry.go
@@ -1,1 +1,75 @@
 package retry
+
+import (
+	"errors"
+	"reflect"
+	"time"
+
+	"github.com/bash360/hupe/pkg/apperror"
+	"github.com/bash360/hupe/pkg/utils"
+)
+
+type Retry struct {
+	interval  time.Duration
+	count     uint
+	operation interface{}
+	args      []any
+}
+
+func New(operation interface{}, args ...any) (*Retry, error) {
+
+	err := validateFunc(operation)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Retry{
+		interval: time.Millisecond * 5,
+		count:    4,
+		args:     args,
+	}, nil
+}
+
+func (r *Retry) SetInterval(interval uint) *Retry {
+	r.interval = time.Millisecond * time.Duration(interval)
+	return r
+}
+
+func (r *Retry) SetCount(count uint) *Retry {
+	r.count = count
+	return r
+}
+
+func (r *Retry) Execute() ([]any, error) {
+
+	operation := reflect.ValueOf(r.operation)
+	var args []reflect.Value
+	if r.args != nil {
+		for _, v := range r.args {
+			args = append(args, reflect.ValueOf(v))
+		}
+	}
+
+	var err error
+	var returnValues []reflect.Value
+
+	for i := 0; i <= int(r.count); i++ {
+
+		returnValues = operation.Call(args)
+		err = returnValues[len(returnValues)-1].Interface().(error)
+
+		if err == nil || errors.As(err, &apperror.NonTransient{}) {
+			break
+		}
+
+		if errors.As(err, &apperror.Transient{}) {
+			time.Sleep(r.interval)
+		}
+
+	}
+
+	payload := utils.ValueToInterface(returnValues[:len(returnValues)-1])
+
+	return payload, err
+
+}
